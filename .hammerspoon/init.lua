7675db61f0cba882b44a057676f0de201685e69a
Shannon Weyrick <sweyrick@nsone.net>
2014-11-21 14:04:33 -0500

    updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,29 +1,38 @@
-local application = require "mjolnir.application"
-local hotkey = require "mjolnir.hotkey"
-local window = require "mjolnir.window"
-local fnutils = require "mjolnir.fnutils"
-local appfinder = require "mjolnir.cmsj.appfinder"
+
+-- application titles
+a_web     = "Google Chrome"
+a_chat    = "Slack"
+a_term    = "iTerm"
+a_pyide   = "PyCharm"
+a_vim     = "MacVim"
+a_irc     = "Textual"
 
 local appkeys = {}
-appkeys["1"] = "Google Chrome"
-appkeys["3"] = "Slack"
-appkeys["4"] = "iTerm"
-appkeys["5"] = "PyCharm"
-appkeys["6"] = "MacVim"
-appkeys["7"] = "Textual"
+appkeys["1"] = a_web
+appkeys["3"] = a_chat
+appkeys["4"] = a_term 
+appkeys["5"] = a_pyide
+appkeys["6"] = a_vim
+appkeys["7"] = a_irc
+
+-- monitors
+lcd = "Color LCD"
+-- home
+acer = "V223W"
+samsung = "SyncMaster"
 
 -- application hotkeys
 for key, title in pairs(appkeys) do 
     -- three key version
-    hotkey.bind({"cmd","alt"}, key, function()
-    local app = appfinder.app_from_name(title)
+    hs.hotkey.bind({"cmd","alt"}, key, function()
+    local app = hs.appfinder.appFromName(title)
     if app then 
         app:activate()
     end
     end)
     -- one key version on numpad
-    hotkey.bind({}, "pad" .. key, function()
-    local app = appfinder.app_from_name(title)
+    hs.hotkey.bind({}, "pad" .. key, function()
+    local app = hs.appfinder.appFromName(title)
     if app then 
         app:activate()
     end
@@ -31,7 +40,14 @@ for key, title in pairs(appkeys) do
 end
 
 -- maximize active window
-hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
-  local win = window.focusedwindow()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+  local win = hs.window.focusedWindow()
   win:maximize(f)
 end)
+
+-- config autoreload
+function reload_config(files)
+    hs.reload()
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.alert.show("Config loaded")
