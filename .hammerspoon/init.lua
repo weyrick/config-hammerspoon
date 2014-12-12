0b87ad58b76ce77e8c2260f0c9960919da324fbe
Shannon Weyrick <sweyrick@nsone.net>
2014-12-12 11:44:17 -0500

    add applescript, gmail, cleanup monitors

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,6 +7,7 @@ a_pyide   = "PyCharm"
 a_vim     = "MacVim"
 a_irc     = "Textual"
 a_hermes  = "Hermes"
+a_mail    = "Mail"
 
 local appkeys = {}
 appkeys["1"] = a_web
@@ -15,20 +16,19 @@ appkeys["4"] = a_term
 appkeys["5"] = a_pyide
 appkeys["6"] = a_vim
 appkeys["7"] = a_irc
+appkeys["8"] = a_mail
 
 -- monitors
 lcd = "Color LCD"
 -- home
-acer = "V223W"
-samsung = "SyncMaster"
 dell = "DELL U2412M"
 -- work
 asus = "VS24A"
 
 -- layouts will be:
 -- 1) n:1 lcd only
--- 2) n:2 lcd plus external (acer or samsung) XX or work
--- 3) n:3 lcd plus 2 external (acer, samsung)
+-- 2) n:2 lcd plus external (dell or asus)
+-- 3) n:1 external only (dell or asus)
 layout_lcd = {
   {a_web, nil, lcd, hs.layout.maximized, nil, nil},
   {a_chat, nil, lcd, hs.layout.maximized, nil, nil},
@@ -37,51 +37,55 @@ layout_lcd = {
   {a_vim, nil, lcd, hs.layout.maximized, nil, nil},
   {a_irc, nil, lcd, hs.layout.maximized, nil, nil},
 }
-function get_layout_2(external)
+function get_layout_ext(external)
     return {
     {a_web, nil, external, hs.layout.maximized, nil, nil},
-    {a_chat, nil, lcd, hs.layout.maximized, nil, nil},
+    {a_chat, nil, external, hs.layout.left75, nil, nil},
     {a_term, nil, external, hs.layout.maximized, nil, nil},
     {a_pyide, nil, external, hs.geometry.rect(0.15, 0, 0.85, 1), nil, nil},
     {a_vim, nil, external, hs.geometry.rect(0.15, 0, 0.85, 1), nil, nil},
-    {a_irc, nil, lcd, hs.layout.maximized, nil, nil},
-    {a_hermes, nil, lcd, nil, nil, nil},
+    {a_irc, nil, external, hs.layout.left75, nil, nil},
+    {a_hermes, nil, external, nil, nil, nil},
     }
 end
-
-layout_3 = {
-  {a_web, nil, acer, hs.layout.maximized, nil, nil},
-  {a_chat, nil, acer, hs.layout.maximized, nil, nil},
-  {a_term, nil, samsung, hs.layout.maximized, nil, nil},
-  {a_pyide, nil, samsung, hs.layout.maximized, nil, nil},
-  {a_vim, nil, samsung, hs.layout.maximized, nil, nil},
-  {a_irc, nil, acer, hs.layout.maximized, nil, nil},
-  {a_hermes, nil, lcd, nil, nil, nil},
-}
-
 
 -- screen watcher 
 function mon_change()
     print("monitor list")
     local count = 0
+    local cur_ext = nil
+    local have_lcd = false
     for k,v in pairs(hs.screen:allScreens()) do 
         print(k, v:name())
         count = count + 1
+        if v:name() == dell then
+            cur_ext = dell
+        elseif v:name() == asus then
+            cur_ext = asus
+        elseif v:name() == lcd then
+            have_lcd = true
+        end
     end
+    print("there are " .. count .. " monitors, have_lcd is " .. tostring(have_lcd) .. " and cur_ext is " .. cur_ext)
     if count == 1 then
-        hs.alert.show("LCD Layout")
-        hs.layout.apply(layout_lcd)
+        if have_lcd then
+            hs.alert.show("LCD Layout")
+            hs.layout.apply(layout_lcd)
+        else
+            hs.alert.show("External Layout")
+            hs.layout.apply(get_layout_ext(cur_ext))
+        end
     elseif count == 2 then
         hs.alert.show("2 Monitor Layout")
-        -- have to figure out which external
-        hs.layout.apply(get_layout_2(dell))
-    elseif count == 3 then
-        hs.layout.apply(layout_3)
-        hs.alert.show("3 Monitor Layout")
+        hs.layout.apply(get_layout_ext(cur_ext))
     else
         hs.alert.show("Unknown Monitor Layout")
     end
 end
+
+-- enable this to switch layouts automatically when monitors change
+-- but i've seen some weirdness where it fires even when they haven't changed
+-- leaving manual with keystroke below for now
 -- hs.screen.watcher.new(mon_change):start()
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
@@ -111,6 +115,25 @@ end
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
   local win = hs.window.focusedWindow()
   win:maximize(f)
+end)
+
+-- function to read a file
+function readAll(file)
+    local f = io.open(file, "rb")
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+-- function to run applescript file
+function runAppleScript(file)
+    script_body = readAll(file)
+    hs.applescript.applescript(script_body)
+end
+
+-- special hotkey to run apple script to switch to gmail
+hs.hotkey.bind({}, "pad2", function()
+    runAppleScript(os.getenv("HOME") .. "/bin/switch_to_gmail.scpt")
 end)
 
 -- config autoreload
